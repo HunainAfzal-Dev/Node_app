c374c04fed02ee156cb3ac10e66f28d33a9b0cfa
HunainAfzal-Dev <hunain.pc933@gmail.com>
2025-08-04 10:46:58 -0700

    `Added API call to fetch tasks and displayed them in the App component`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,20 @@ import { useEffect } from 'react'
 
 function App() {
   const [jokes, setJokes] = useState([])
+  const [tasks, setTasks] = useState([])
   useEffect (() => { 
     axios.get('/api/jokes',) 
     .then((response) => {
     console.log(response.data);
       setJokes(response.data)
+    })
+    .catch((error) => {
+      console.error(error);
+      });
+    axios.get('/api/tasks',) 
+    .then((response) => {
+    console.log(response.data);
+      setTasks(response.data)
     })
     .catch((error) => {
       console.error(error);
@@ -26,6 +35,15 @@ function App() {
       <div key={joke.id}>
         <h2> {joke.title} </h2>
         <p> {joke.joke} </p>
+      </div>
+    ))}
+    <div>
+      tasks: {tasks.length}
+    </div>
+    {tasks.map((task ) => (
+      <div key={task.id}>
+        <h2> {task.title} </h2>
+        <p> {task.joke} </p>
       </div>
     ))}
     </>
