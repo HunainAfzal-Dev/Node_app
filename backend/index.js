6db7eac0c93dbd4edd74b3303f5443f73a0ce986
HunainAfzal-Dev <hunain.pc933@gmail.com>
2025-08-18 22:42:42 +0500

    `Readded removed GET /api/about route and kept the rest of the code unchanged.`

--- backend/index.js
+++ backend/index.js
@@ -29,14 +29,14 @@ app.get('/api/jokes', (req, res) => {
     res.json(jokes);
 });
 
-// // GET: About Page
-// app.get('/api/about', (req, res) => {
-//     res.send({
-//         message: "This is the about page for our awesome Express server.",
-//         version: "1.0.0",
-//         author: "Your Name"
-//     });
-// });
+// GET: About Page
+app.get('/api/about', (req, res) => {
+    res.send({
+        message: "This is the about page for our awesome Express server.",
+        version: "1.0.0",
+        author: "Your Name"
+    });
+});
 
 // GET: Contact Info
 app.get('/api/contact', (req, res) => {
